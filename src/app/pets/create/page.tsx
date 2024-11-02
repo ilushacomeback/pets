7bfdafcf9be8b9e598313c551f187e0038288008
ilushacomeback <ilya.rudenko.2105@mail.ru>
2024-11-02 20:49:06 +0300

    test env

--- src/app/pets/create/page.tsx
+++ src/app/pets/create/page.tsx
@@ -5,9 +5,10 @@ import { FieldName } from '@/components/ui/forms/create';
 import { FieldBirthday } from '@/components/ui/forms/create';
 import { Cities } from '@/components/Cities';
 import { Submit } from '@/components/ui/forms/create';
+import { FormEvent } from 'react';
 
 export default function Create() {
-  const handleSubmit = (e: any) => {
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     const data = new FormData(e.target);
     console.log(data.get('photos'));
